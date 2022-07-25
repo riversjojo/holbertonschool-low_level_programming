6df249663c63ff7575b55d484a70380dcf55db42
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-25 16:15:49 +0000

    hello_world

---
+++ hello_world/6-size.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+*
+*
+*/
+
+int main(void)
+{
+	printf("Size of a char:" "%lu\n", sizeof(char));
+	printf("Size of an int:" "%lu\n", sizeof(int));
+	printf("Size of a long int:""%lu\n", sizeof(float));
+	printf("Size of a float""%lu", sizeof(double));
+
+}
