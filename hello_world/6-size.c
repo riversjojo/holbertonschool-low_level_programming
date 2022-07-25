63ffbf9b0f899f05f578a5f29f9877222abb3b52
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-25 16:19:02 +0000

    hello_world

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -6,9 +6,9 @@
 
 int main(void)
 {
-	printf("Size of a char:" "%lu\n", sizeof(char));
-	printf("Size of an int:" "%lu\n", sizeof(int));
-	printf("Size of a long int:""%lu\n", sizeof(float));
-	printf("Size of a float""%lu", sizeof(double));
+	printf("Size of a char: " "%lu\n", sizeof(char)" byte(s)");
+	printf("Size of an int: " "%lu\n", sizeof(int));
+	printf("Size of a long int: ""%lu\n", sizeof(float));
+	printf("Size of a float: ""%lu", sizeof(double));
 
 }
