4260b876e62c0623b5d29d5f144225b9dcbdc79c
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-25 15:11:28 +0000

    hello_world

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 /**
-* main - script that prints text using puts
-* Return: 0.
-*/ 
+ * main - script that prints text using puts
+ * Return: 0.
+ */ 
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
