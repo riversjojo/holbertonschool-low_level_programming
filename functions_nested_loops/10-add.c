834bc3f02f25bb14d68703065048a729adefb0e0
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-27 18:44:41 +0000

    nested loops

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+*
+*
+*
+*/
+
+int add(int a, int b)
+{
+   int x = a + b;
+
+   return (x);
+
+
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,3 +6,4 @@ int _isalpha(int c);
 int print_sign(int n);
 
 int print_last_digit(int);
+int add(int a, int b);
