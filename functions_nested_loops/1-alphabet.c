9d9ae1c471960cfdc156d42e9f42390a765656ef
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-27 15:31:43 +0000

    nested loops

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/** 
+* main - function that prints the alphabet
+* Return: 0.
+*/ 
+
+int main(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+		_puthchar('\n');
+	return (0);
+
+
+}
