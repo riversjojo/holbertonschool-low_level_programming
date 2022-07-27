7dd1d735037e3268ea381bbd38dd0da194beb54f
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-27 22:51:15 +0000

    nested loops

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -1,10 +1,12 @@
 #include "main.h"
 /**
-*
+* _abs - sxript that returns absolute value of a number
+* @x: variable used for script
+* Return: x
 *
 */
 
-int _abs(int x)
+int _abs (int x)
 {
 	if (x < 0)
 	{
