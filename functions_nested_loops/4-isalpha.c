52d9409f93b8f37160fef7521516ac7a80e99abb
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-27 16:38:25 +0000

    nested loops

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _isalpha - function that checks for alphabetical characters
+* @c: variable used to check
+* Return: 0.
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
+
+
+}
+vagr
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,4 +2,4 @@ int _putchar(char);
 void print_alphabet(void);
 
 int _islower(int c);
-
+int _isalpha(int c);
