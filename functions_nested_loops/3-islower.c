9d1c0db87852f152944ba6652bd426d9528433f0
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-27 16:29:06 +0000

    nested loops

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
-*
-*
+* main - function that checks for lowercase characters
+* Return: 0.
 */
 
 int _islower(int c)
 {
-	if(c >= 'a' && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
