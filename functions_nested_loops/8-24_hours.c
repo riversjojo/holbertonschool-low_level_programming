79d913c1152d07b3f49bf288257397f248f1ca01
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-27 22:24:29 +0000

    nested loops

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 /**
 *
 *
@@ -8,7 +9,7 @@ void jack_bauer(void)
 {
 	int x, y;
 
-	for (x = 0; x < 6; t++)
+	for (x = 0; x < 6; x++)
 	{
 		for (y = 0; y < 10; y++)
 		{
