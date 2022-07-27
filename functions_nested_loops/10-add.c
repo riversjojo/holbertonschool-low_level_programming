4a5ae1a96bbe6e96640751eb3f17024bdd9d3973
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-27 18:48:58 +0000

    nested loops

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
-*
-*
-*
+* add -function that adds two numbers
+* @a: variable that will be added
+* @b: variable that will be added
+* Return: x.
 */
 
 int add(int a, int b)
 {
-   int x = a + b;
+	int x = a + b;
 
-   return (x);
+	return (x);
 
 
 }
