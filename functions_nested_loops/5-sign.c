6ca158cce2e766c36f1ede78ca86d580c93a6240
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-27 17:21:19 +0000

    nested loops

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -3,13 +3,13 @@
 * print_sign - function that prints sign of a number
 * @n: variable used
 * Return: 1, 0 or -1.
-*/ 
+*/
 
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-	_putchar(43);	
+	_putchar(43);
 	return (1);
 	}
 	else if (n < 0)
@@ -19,7 +19,7 @@ int print_sign(int n)
 	}
 	else
 	_putchar('0');
-	return(0);
+	return (0);
 
 
 }
