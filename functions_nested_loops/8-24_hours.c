085bfe98fbab8a4320a2b074d6a06d04cfbfb390
riversjojo <giovanni.rivera.pagan@gmail.com>
2022-07-27 22:23:14 +0000

    nested loops

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+*
+*
+*/
+
+void jack_bauer(void)
+{
+	int x, y;
+
+	for (x = 0; x < 6; t++)
+	{
+		for (y = 0; y < 10; y++)
+		{
+			printf(x);
+			printf(y);
+		}
+	}
+
+
+
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -6,5 +6,6 @@ int _isalpha(int c);
 int print_sign(int n);
 
 int print_last_digit(int x);
+void jack_bauer(void);
 void times_table(void);
 int add(int a, int b);
